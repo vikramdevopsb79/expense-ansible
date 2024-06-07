730f15cf660f210bbcec323a53bbb3f13ac912f4
sai <kuppalasaivikram@gmail.com>
2024-06-07 08:53:59 +0530

    env prod added properly

--- run.sh
+++ run.sh
@@ -1,4 +1,1 @@
- echo component is $component
- echo password is $SSH_PASSWORD
- echo env is $env
 ansible-playbook -i ${component}-${env}.vikramdevops.tech, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env}  expense.yml
