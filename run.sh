c31be3ef3022877ce9296c40f8eac4629b7c9097
sai <kuppalasaivikram@gmail.com>
2024-06-07 09:23:38 +0530

    GOCD TEMPLATES ADDING PIPELINE TO SKIP STAGES

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+if [ "$pipeline" == "true" ]; then
+  exit
+fi
 ansible-playbook -i ${component}-${env}.vikramdevops.tech, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env}  expense.yml
